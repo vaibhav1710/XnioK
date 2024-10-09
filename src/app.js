a17db750c80f72559e997f02696079eacfd858bc
Vaibhav Singh <singhvaibhav1710@gmail.com>
2024-10-09 21:03:54 +0530

    Established a connection to MongoDB Atlas

--- src/app.js
+++ src/app.js
@@ -1,7 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
- 
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DATABASE_URL); 
+        console.log(`DataBase Connected: ${conn.connection.host}`); 
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('Hello World! From KoinX task');
